d65ab1320060d8fba9e1acdaf1287d4fad6c4b11
boraty14 <bratygame14@gmail.com>
2024-10-24 17:18:49 +0300

    layout adjustment

--- Assets/Brui/Components/NodeLayout.cs
+++ Assets/Brui/Components/NodeLayout.cs
@@ -1,17 +1,13 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Brui.Components
 {
     [RequireComponent(typeof(NodeTransform))]
-    [ExecuteAlways]
     public class NodeLayout : MonoBehaviour
     {
         public ENodeLayout layoutType;
         public bool isReverse;
         public NodeTransform NodeTransform { get; private set; }
-
-        private readonly List<NodeTransform> _childNodes;
         
         private void OnValidate()
         {
