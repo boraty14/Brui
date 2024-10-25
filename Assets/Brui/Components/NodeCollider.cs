f459afa354e7159858d8ac6f2cc832e3bf1709bb
boraty14 <bratygame14@gmail.com>
2024-10-25 03:23:15 +0300

    added colliders

--- Assets/Brui/Components/NodeCollider.cs
+++ Assets/Brui/Components/NodeCollider.cs
@@ -1,16 +1,24 @@
+using System;
 using UnityEngine;
 
 namespace Brui.Components
 {
+    [RequireComponent(typeof(NodeTransform))]
     [ExecuteAlways]
     public class NodeCollider : MonoBehaviour
     {
         [SerializeField] private ENodeCollider _nodeColliderType;
+        [SerializeField] private NodeColliderReferences _colliderReferences;
         public NodeTransform NodeTransform { get; private set; }
 
         private ENodeCollider _latestNodeColliderType;
         private Vector2 _latestNodeSize;
-        
+
+        private float _circleColliderSize;
+        private Vector2 _boxColliderSize;
+        private Vector2 _capsuleColliderSize;
+        private CapsuleDirection2D _capsuleDirection;
+
         private void OnValidate()
         {
             SetComponents();
@@ -25,6 +33,109 @@ namespace Brui.Components
         {
             NodeTransform = GetComponent<NodeTransform>();
         }
+
+        private void Update()
+        {
+            if (_latestNodeColliderType == _nodeColliderType &&
+                _latestNodeSize == NodeTransform.NodeSize &&
+                IsColliderSizeSame())
+            {
+                return;
+            }
+
+            var nodeSize = NodeTransform.NodeSize;
+            switch (_nodeColliderType)
+            {
+                case ENodeCollider.Box:
+                    if (_colliderReferences.CapsuleCollider != null)
+                    {
+                        DestroyCollider(_colliderReferences.CapsuleCollider);
+                    }
+
+                    if (_colliderReferences.CircleCollider != null)
+                    {
+                        DestroyCollider(_colliderReferences.CircleCollider);
+                    }
+
+                    if (_colliderReferences.BoxCollider == null)
+                    {
+                        _colliderReferences.BoxCollider = gameObject.AddComponent<BoxCollider2D>();
+                    }
+
+                    _colliderReferences.BoxCollider.size = nodeSize;
+                    _boxColliderSize = nodeSize;
+                    break;
+                case ENodeCollider.Circle:
+                    if (_colliderReferences.CapsuleCollider != null)
+                    {
+                        DestroyCollider(_colliderReferences.CapsuleCollider);
+                    }
+
+                    if (_colliderReferences.BoxCollider != null)
+                    {
+                        DestroyCollider(_colliderReferences.BoxCollider);
+                    }
+
+                    if (_colliderReferences.CircleCollider == null)
+                    {
+                        _colliderReferences.CircleCollider = gameObject.AddComponent<CircleCollider2D>();
+                    }
+
+                    _colliderReferences.CircleCollider.radius = nodeSize.x * 0.5f;
+                    _circleColliderSize = nodeSize.x  * 0.5f;
+                    break;
+                case ENodeCollider.Capsule:
+                    if (_colliderReferences.CircleCollider != null)
+                    {
+                        DestroyCollider(_colliderReferences.CircleCollider);
+                    }
+
+                    if (_colliderReferences.BoxCollider != null)
+                    {
+                        DestroyCollider(_colliderReferences.BoxCollider);
+                    }
+
+                    if (_colliderReferences.CapsuleCollider == null)
+                    {
+                        _colliderReferences.CapsuleCollider = gameObject.AddComponent<CapsuleCollider2D>();
+                    }
+
+                    _colliderReferences.CapsuleCollider.direction = nodeSize.x > nodeSize.y
+                        ? CapsuleDirection2D.Horizontal
+                        : CapsuleDirection2D.Vertical;
+                    _colliderReferences.CapsuleCollider.size = nodeSize;
+                    _capsuleColliderSize = nodeSize;
+                    break;
+            }
+
+            _latestNodeColliderType = _nodeColliderType;
+            _latestNodeSize = NodeTransform.NodeSize;
+        }
+
+        private bool IsColliderSizeSame()
+        {
+            return _nodeColliderType switch
+            {
+                ENodeCollider.Box => _boxColliderSize == _colliderReferences.BoxCollider.size,
+                ENodeCollider.Circle => Mathf.Approximately(_circleColliderSize,
+                    _colliderReferences.CircleCollider.radius),
+                ENodeCollider.Capsule => _capsuleColliderSize == _colliderReferences.CapsuleCollider.size &&
+                                         _capsuleDirection == _colliderReferences.CapsuleCollider.direction,
+                _ => false
+            };
+        }
+
+        private void DestroyCollider(Component colliderComponent)
+        {
+            if (Application.isPlaying)
+            {
+                Destroy(colliderComponent);
+            }
+            else
+            {
+                DestroyImmediate(colliderComponent);
+            }
+        }
     }
 
     public enum ENodeCollider
@@ -32,5 +143,13 @@ namespace Brui.Components
         Box,
         Circle,
         Capsule
+    }
+
+    [Serializable]
+    public class NodeColliderReferences
+    {
+        public BoxCollider2D BoxCollider;
+        public CircleCollider2D CircleCollider;
+        public CapsuleCollider2D CapsuleCollider;
     }
 }
