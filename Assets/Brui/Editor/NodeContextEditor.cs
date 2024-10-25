619ec75f63d7e16e11222451c35270f4bbd36bb0
boraty14 <bratygame14@gmail.com>
2024-10-25 08:48:59 +0300

    improve node creation

--- Assets/Brui/Editor/NodeContextEditor.cs
+++ Assets/Brui/Editor/NodeContextEditor.cs
@@ -6,8 +6,8 @@ namespace Brui.Editor
 {
     public class NodeContextEditor
     {
-        [MenuItem("GameObject/Create Node", false, 0)]
-        static void CustomAction()
+        [MenuItem("GameObject/Node/Create Node", false, 0)]
+        static void CreateNode()
         {
             if (Selection.activeGameObject != null)
             {
@@ -24,6 +24,69 @@ namespace Brui.Editor
                 Selection.activeGameObject = newChild;
 
                 newChild.AddComponent<NodeTransform>();
+            }
+        }
+        
+        [MenuItem("GameObject/Node/Create Node Image", false, 0)]
+        static void CreateNodeImage()
+        {
+            if (Selection.activeGameObject != null)
+            {
+                // Create a new empty GameObject
+                GameObject newChild = new GameObject("NewNode");
+
+                // Set the newly created object as a child of the selected GameObject
+                newChild.transform.parent = Selection.activeGameObject.transform;
+
+                // Reset the child's local position to (0,0,0)
+                newChild.transform.localPosition = Vector3.zero;
+
+                // Optionally, select the newly created GameObject in the Hierarchy
+                Selection.activeGameObject = newChild;
+
+                newChild.AddComponent<NodeImage>();
+            }
+        }
+        
+        [MenuItem("GameObject/Node/Create Node Button", false, 0)]
+        static void CreateNodeButton()
+        {
+            if (Selection.activeGameObject != null)
+            {
+                // Create a new empty GameObject
+                GameObject newChild = new GameObject("NewNode");
+
+                // Set the newly created object as a child of the selected GameObject
+                newChild.transform.parent = Selection.activeGameObject.transform;
+
+                // Reset the child's local position to (0,0,0)
+                newChild.transform.localPosition = Vector3.zero;
+
+                // Optionally, select the newly created GameObject in the Hierarchy
+                Selection.activeGameObject = newChild;
+
+                newChild.AddComponent<NodeButton>();
+            }
+        }
+        
+        [MenuItem("GameObject/Node/Create Node Scroll", false, 0)]
+        static void CreateNodeScroll()
+        {
+            if (Selection.activeGameObject != null)
+            {
+                // Create a new empty GameObject
+                GameObject newChild = new GameObject("NewNode");
+
+                // Set the newly created object as a child of the selected GameObject
+                newChild.transform.parent = Selection.activeGameObject.transform;
+
+                // Reset the child's local position to (0,0,0)
+                newChild.transform.localPosition = Vector3.zero;
+
+                // Optionally, select the newly created GameObject in the Hierarchy
+                Selection.activeGameObject = newChild;
+
+                newChild.AddComponent<NodeScroll>();
             }
         }
     }
